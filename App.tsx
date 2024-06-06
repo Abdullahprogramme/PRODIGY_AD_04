56785b5cf1943b6d9b9a92bb1b93a90476ac5e32
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-06-06 22:20:10 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
 import GameBoard from './src/Components/GameBoard';
+import ComputerGameBoard from './src/Components/ComputerGameBoard';
 
 const App: React.FC = () => {
+  const [mode, serMode] = useState<'1v1' | '1vComputer'>('1v1'); // '1v1' or '1vComputer
   const [gameOver, setGameOver] = useState(false);
   const [winner, setWinner] = useState<'X' | 'O' | 'Draw' | null>(null);
 
@@ -16,11 +18,27 @@ const App: React.FC = () => {
     setWinner(null);
   };
 
+  const switchMode = () => {
+    serMode(mode === '1v1' ? '1vComputer' : '1v1');
+    restartGame();
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={'dark-content'} />
       <Text style={styles.title}>TicTacToe</Text>
-      <GameBoard onGameOver={handleGameOver} gameOver={gameOver} />
+
+      <TouchableOpacity style={styles.switchButton} onPress={switchMode}>
+        <Text>Switch to {mode === '1v1' ? '1vComputer' : '1v1'}</Text>
+      </TouchableOpacity>
+
+
+      {mode === '1v1' ? (
+        <GameBoard onGameOver={handleGameOver} gameOver={gameOver} />
+      ) : (
+        <ComputerGameBoard onGameOver={handleGameOver} gameOver={gameOver} />
+      )}
+
       {gameOver && (
         <View style={styles.resultContainer}>
           <Text style={styles.resultText}>{winner !== 'Draw' ? `Winner: ${winner}` : 'It`s a Draw'}</Text>
@@ -60,6 +78,14 @@ const styles = StyleSheet.create({
     padding: 10,
     color: 'white',
     alignItems: 'center',
+  },
+  switchButton: {
+    backgroundColor: '#841584',
+    borderRadius: 5,
+    padding: 10,
+    color: 'white',
+    alignItems: 'center',
+    marginBottom: 10,
   },
 });
 
